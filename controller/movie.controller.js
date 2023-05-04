4d233d35d70d6b374c626cf3ac8173e27d2d6e16
Vytenis <vytenis.kondrackis@gmail.com>
2023-05-04 21:08:57 +0300

    deleted unused code

--- controller/movie.controller.js
+++ controller/movie.controller.js
@@ -46,23 +46,4 @@ export const getMovies = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-
-  // const q = req.query;
-  // const filters = {
-  //   ...(q.userId && { userId: q.userId }),
-  //   ...(q.cat && { cat: q.cat }),
-  //   ...((q.min || q.max) && {
-  //     price: {
-  //       ...(q.min && { $gt: q.min }),
-  //       ...(q.max && { $lt: q.max }),
-  //     },
-  //   }),
-  //   ...(q.search && { title: { $regex: q.search, $options: 'i' } }),
-  // };
-  // try {
-  //   const gigs = await Gig.find(filters).sort({ [q.sort]: -1 });
-  //   res.status(200).send(gigs);
-  // } catch (err) {
-  //   next(err);
-  // }
 };
